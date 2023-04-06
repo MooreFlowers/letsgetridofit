6fc90901b593e6b71913b739b20e3b9e1bd36351
Moore Flowers <mooreflowers@Moores-MacBook-Pro.local>
2023-04-05 22:36:37 -0400

    hostbuild 0.1

---
+++ build/precache-manifest.ef60ea5ff986f0e0b355f9b12655ea38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a113b6a61830cea50c8495a835bf19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3e32b5f25d4c0095b4",
+    "url": "/static/css/main.f8dd4836.chunk.css"
+  },
+  {
+    "revision": "f3dbeded1939efce6ea4",
+    "url": "/static/js/2.ccb4e83e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ccb4e83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3e32b5f25d4c0095b4",
+    "url": "/static/js/main.44a01ad7.chunk.js"
+  },
+  {
+    "revision": "1c6798b596aadcc02b8e",
+    "url": "/static/js/runtime-main.36c1e16a.js"
+  }
+]);
